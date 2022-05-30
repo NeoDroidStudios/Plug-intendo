8479d20927a3f4ae4033d8aae3b43dab7af483bb
Andromeda <79194733+www-andromeda@users.noreply.github.com>
2022-05-31 00:36:51 +0200

    Add files via upload

---
+++ plug-intendo/background.js
@@ -0,0 +1,274 @@
+function addPhoto() {
+
+  ///TOP LEFT
+
+  //Column 1
+
+  let imgTopLeft1 = document.createElement("img");
+  imgTopLeft1.src = "https://24.media.tumblr.com/d3e5f235ee5ce45f997a099deace3eb6/tumblr_mr7107Zcaa1syx43eo1_400.gif";
+  imgTopLeft1.width = "150";
+  imgTopLeft1.style = "position:fixed; top:0px; left:10px;";
+  document.body.appendChild(imgTopLeft1);
+
+  let imgTopLeft2 = document.createElement("img");
+  imgTopLeft2.src = "https://i.gifer.com/origin/3a/3a81c3d5d8b2b982f9107cd1ca32994e_w200.gif";
+  imgTopLeft2.width = "150";
+  imgTopLeft2.style = "position:fixed; top:100px; left:10px;";
+  document.body.appendChild(imgTopLeft2);
+
+  let imgTopLeft3 = document.createElement("img");
+  imgTopLeft3.src = "https://64.media.tumblr.com/13bbd01b2afe88b6825eb0b32a0c5116/tumblr_myupozyC0G1qfqgb9o1_1280.gifv";
+  imgTopLeft3.width = "150";
+  imgTopLeft3.style = "position:fixed; top:250px; left:10px;";
+  document.body.appendChild(imgTopLeft3);
+
+  let imgTopLeft4 = document.createElement("img");
+  imgTopLeft4.src = "https://thumbs.gfycat.com/PlayfulNegativeBustard-max-1mb.gif";
+  imgTopLeft4.width = "150";
+  imgTopLeft4.style = "position:fixed; top:400px; left:10px;";
+  document.body.appendChild(imgTopLeft4);
+
+  //Column 2
+
+  let imgTopLeft5 = document.createElement("img");
+  imgTopLeft5.src = "https://giffiles.alphacoders.com/163/163060.gif";
+  imgTopLeft5.width = "150";
+  imgTopLeft5.style = "position:fixed; top:0px; left:150px;";
+  document.body.appendChild(imgTopLeft5);
+
+  let imgTopLeft6 = document.createElement("img");
+  imgTopLeft6.src = "https://data.whicdn.com/images/288560761/original.gif";
+  imgTopLeft6.width = "150";
+  imgTopLeft6.style = "position:fixed; top:100px; left:150px;";
+  document.body.appendChild(imgTopLeft6);
+
+  let imgTopLeft7 = document.createElement("img");
+  imgTopLeft7.src = "https://66.media.tumblr.com/tumblr_maorjmtDaY1rfjowdo1_500.gif";
+  imgTopLeft7.width = "150";
+  imgTopLeft7.style = "position:fixed; top:350px; left:150px;";
+  document.body.appendChild(imgTopLeft7);
+
+  //Column 3
+
+  let imgTopLeft8 = document.createElement("img");
+  imgTopLeft8.src = "https://thumbs.gfycat.com/UnrulyGenuineBillygoat-max-1mb.gif";
+  imgTopLeft8.width = "150";
+  imgTopLeft8.style = "position:fixed; top:0px; left:300px;";
+  document.body.appendChild(imgTopLeft8);
+
+  let imgTopLeft9 = document.createElement("img");
+  imgTopLeft9.src = "https://i.pinimg.com/originals/f0/86/bf/f086bf3d490cddd0c739f002fd993d5c.gif";
+  imgTopLeft9.width = "150";
+  imgTopLeft9.style = "position:fixed; top:100px; left:300px;";
+  document.body.appendChild(imgTopLeft9);
+
+  let imgTopLeft10 = document.createElement("img");
+  imgTopLeft10.src = "https://66.media.tumblr.com/tumblr_mb6bqcRj8g1rfjowdo1_500.gif";
+  imgTopLeft10.width = "150";
+  imgTopLeft10.style = "position:fixed; top:250px; left:300px;";
+  document.body.appendChild(imgTopLeft10);
+
+  let imgTopLeft11 = document.createElement("img");
+  imgTopLeft11.src = "https://i.makeagif.com/media/4-25-2017/VmUnjo.gif";
+  imgTopLeft11.width = "150";
+  imgTopLeft11.style = "position:fixed; top:425px; left:300px;";
+  document.body.appendChild(imgTopLeft11);
+
+  //Column 4
+
+  let imgTopLeft12 = document.createElement("img");
+  imgTopLeft12.src = "https://supportivy.com/wp-content/uploads/2020/08/pixel-art-nintendo-Idees-designs-photo-23.gif";
+  imgTopLeft12.width = "150";
+  imgTopLeft12.style = "position:fixed; top:0px; left:450px;";
+  document.body.appendChild(imgTopLeft12);
+
+  let imgTopLeft13 = document.createElement("img");
+  imgTopLeft13.src = "https://i.pinimg.com/originals/d6/21/7b/d6217be6972cc52270a135cec224e061.gif";
+  imgTopLeft13.width = "150";
+  imgTopLeft13.style = "position:fixed; top:100px; left:450px;";
+  document.body.appendChild(imgTopLeft13);
+
+  let imgTopLeft14 = document.createElement("img");
+  imgTopLeft14.src = "https://i.gifer.com/origin/1e/1e9a40d0270220aee8954b50ec70a76f_w200.gif";
+  imgTopLeft14.width = "150";
+  imgTopLeft14.style = "position:fixed; top:150px; left:450px;";
+  document.body.appendChild(imgTopLeft14);
+
+  let imgTopLeft15 = document.createElement("img");
+  imgTopLeft15.src = "https://64.media.tumblr.com/tumblr_lnq14iCprj1qkd6mto1_500.gifv";
+  imgTopLeft15.width = "150";
+  imgTopLeft15.style = "position:fixed; top:300px; left:450px;";
+  document.body.appendChild(imgTopLeft15);
+
+  let imgTopLeft16 = document.createElement("img");
+  imgTopLeft16.src = "https://giffiles.alphacoders.com/163/163000.gif";
+  imgTopLeft16.width = "150";
+  imgTopLeft16.style = "position:fixed; top:400px; left:450px;";
+  document.body.appendChild(imgTopLeft16);
+
+  ///TOP RIGHT
+
+  //Column 1
+
+  let imgTopRight1 = document.createElement("img");
+  imgTopRight1.src = "https://c.tenor.com/0GRl16naN8YAAAAi/pokemon-nintendo.gif";
+  imgTopRight1.width = "150";
+  imgTopRight1.style = "position:fixed; top:0px; right:10px;";
+  document.body.appendChild(imgTopRight1);
+
+  let imgTopRight2 = document.createElement("img");
+  imgTopRight2.src = "https://www.icegif.com/wp-content/uploads/2022/03/icegif-482.gif";
+  imgTopRight2.width = "150";
+  imgTopRight2.style = "position:fixed; top:150px; right:10px;";
+  document.body.appendChild(imgTopRight2);
+
+  let imgTopRight3 = document.createElement("img");
+  imgTopRight3.src = "https://64.media.tumblr.com/560aa2bab42d60b3322097b16b789970/tumblr_mvawhgM3DB1rby4vso1_500.gifv";
+  imgTopRight3.width = "150";
+  imgTopRight3.style = "position:fixed; top:300px; right:10px;";
+  document.body.appendChild(imgTopRight3);
+
+  let imgTopRight4 = document.createElement("img");
+  imgTopRight4.src = "https://thumbs.gfycat.com/AlarmingInstructiveAmericanbobtail-size_restricted.gif";
+  imgTopRight4.width = "150";
+  imgTopRight4.style = "position:fixed; top:425px; right:10px;";
+  document.body.appendChild(imgTopRight4);
+
+  //Column 2
+
+  let imgTopRight5 = document.createElement("img");
+  imgTopRight5.src = "https://i.gifer.com/origin/ed/edab6122adcb8868bf28208cc9e9bf5d_w200.gif";
+  imgTopRight5.width = "75";
+  imgTopRight5.style = "position:fixed; top:0px; right:200px;";
+  document.body.appendChild(imgTopRight5);
+
+  let imgTopRight6 = document.createElement("img");
+  imgTopRight6.src = "https://media.baamboozle.com/uploads/images/526117/1635505537_51555_gif-url.gif";
+  imgTopRight6.width = "150";
+  imgTopRight6.style = "position:fixed; top:150px; right:150px;";
+  document.body.appendChild(imgTopRight6);
+
+  let imgTopRight7 = document.createElement("img");
+  imgTopRight7.src = "https://64.media.tumblr.com/a810323b60df1ae04018bee38ff09119/tumblr_ml9jck7HQl1rby4vso1_r1_500.gifv";
+  imgTopRight7.width = "150";
+  imgTopRight7.style = "position:fixed; top:300px; right:150px;";
+  document.body.appendChild(imgTopRight7);
+
+  let imgTopRight8 = document.createElement("img");
+  imgTopRight8.src = "https://i.kym-cdn.com/photos/images/original/000/906/639/d5e.gif";
+  imgTopRight8.width = "150";
+  imgTopRight8.style = "position:fixed; top:475px; right:150px;";
+  document.body.appendChild(imgTopRight8);
+
+  //Column 3
+
+  let imgTopRight9 = document.createElement("img");
+  imgTopRight9.src = "https://i.pinimg.com/originals/92/27/2e/92272e170fb992e9bfbe7188a420c859.gif";
+  imgTopRight9.width = "150";
+  imgTopRight9.style = "position:fixed; top:0px; right:300px;";
+  document.body.appendChild(imgTopRight9);
+
+  let imgTopRight10 = document.createElement("img");
+  imgTopRight10.src = "https://i.pinimg.com/originals/1b/96/64/1b966476fe97b8f4f3be53fbf4c5b6fe.gif";
+  imgTopRight10.width = "150";
+  imgTopRight10.style = "position:fixed; top:175px; right:300px;";
+  document.body.appendChild(imgTopRight10);
+
+  let imgTopRight11 = document.createElement("img");
+  imgTopRight11.src = "https://giffiles.alphacoders.com/162/162789.gif";
+  imgTopRight11.width = "150";
+  imgTopRight11.style = "position:fixed; top:360px; right:300px;";
+  document.body.appendChild(imgTopRight11);
+
+  //Colunm 4
+
+  let imgTopRight444 = document.createElement("img");
+  imgTopRight444.src = "https://www.photofunky.net/output/image/2/2/3/e/223e6b/photofunky.gif";
+  imgTopRight444.width = "150";
+  imgTopRight444.style = "position:fixed; top:0px; right:475px;";
+  document.body.appendChild(imgTopRight444);
+
+  let imgTopRight445 = document.createElement("img");
+  imgTopRight445.src = "https://supermario3dworld.nintendo.com/assets/img/home/features/coin.gif";
+  imgTopRight445.width = "150";
+  imgTopRight445.style = "position:fixed; top:200px; right:475px;";
+  document.body.appendChild(imgTopRight445);
+
+  let imgTopRight446 = document.createElement("img");
+  imgTopRight446.src = "https://itsclio.files.wordpress.com/2018/09/original.gif";
+  imgTopRight446.width = "150";
+  imgTopRight446.style = "position:fixed; top:375px; right:475px;";
+  document.body.appendChild(imgTopRight446);
+
+  //Larg
+
+  //BOTTOM LEFT
+  let imgBotLeft1 = document.createElement("img");
+  imgBotLeft1.src = "https://66.media.tumblr.com/tumblr_mbc2ctcRTS1rfjowdo1_500.gif";
+  imgBotLeft1.width = "150";
+  imgBotLeft1.style = "position:fixed; bottom:0px; left:0px;";
+  document.body.appendChild(imgBotLeft1);
+
+  let imgBotLeft2 = document.createElement("img");
+  imgBotLeft2.src = "https://i.gifer.com/QHa.gif";
+  imgBotLeft2.width = "150";
+  imgBotLeft2.style = "position:fixed; bottom:0px; left:150px;";
+  document.body.appendChild(imgBotLeft2);
+
+  let imgBotLeft3 = document.createElement("img");
+  imgBotLeft3.src = "https://66.media.tumblr.com/9eb0d40f8191fe2379a72b773673cd66/tumblr_mfmoe6fU6z1rfjowdo1_500.gif";
+  imgBotLeft3.width = "150";
+  imgBotLeft3.style = "position:fixed; bottom:0px; left:300px;";
+  document.body.appendChild(imgBotLeft3);
+
+  let imgBotLeft4 = document.createElement("img");
+  imgBotLeft4.src = "https://i.pinimg.com/originals/95/d8/e4/95d8e45a674449c7afaa58f9bb3818fb.gif";
+  imgBotLeft4.width = "150";
+  imgBotLeft4.style = "position:fixed; bottom:0px; left:500px;";
+  document.body.appendChild(imgBotLeft4);
+
+  let imgBotLeft5 = document.createElement("img");
+  imgBotLeft5.src = "https://i.gifer.com/ZcsO.gif";
+  imgBotLeft5.width = "150";
+  imgBotLeft5.style = "position:fixed; bottom:0px; left:650px;";
+  document.body.appendChild(imgBotLeft5);
+
+  //BOTTOM RIGHT
+  let imgBotRight1 = document.createElement("img");
+  imgBotRight1.src = "https://i.gifer.com/origin/dc/dc689a73ade59a2c882a01afedc2f441.gif";
+  imgBotRight1.width = "150";
+  imgBotRight1.style = "position:fixed; bottom:0px; right:0px;";
+  document.body.appendChild(imgBotRight1);
+
+  let imgBotRight2 = document.createElement("img");
+  imgBotRight2.src = "http://assets.stickpng.com/images/58888515bc2fc2ef3a186097.png";
+  imgBotRight2.width = "20";
+  imgBotRight2.style = "position:fixed; bottom:20px; right:85px;";
+  document.body.appendChild(imgBotRight2);
+
+  let imgBotRight3 = document.createElement("img");
+  imgBotRight3.src = "https://66.media.tumblr.com/tumblr_mc247nrpWQ1rfjowdo1_500.gif";
+  imgBotRight3.width = "150";
+  imgBotRight3.style = "position:fixed; bottom:0px; right:150px;";
+  document.body.appendChild(imgBotRight3);
+
+  let imgBotRight4 = document.createElement("img");
+  imgBotRight4.src = "https://www.gifsanimes.com/data/media/1388/nintendo-image-animee-0016.gif";
+  imgBotRight4.width = "150";
+  imgBotRight4.style = "position:fixed; bottom:0px; right:300px;";
+  document.body.appendChild(imgBotRight4);
+
+}
+
+chrome.action.onClicked.addListener((tab) => {
+
+  if (!tab.url.includes("chrome://")) {
+
+    chrome.scripting.executeScript({
+
+      target: { tabId: tab.id },
+      function: addPhoto
+
+    });
+  }
+});
